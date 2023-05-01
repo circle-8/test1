3ac0a06982e6184da8ae9893c466c8a7292522e5
NewtonGauss <germanmerkel9@gmail.com>
2023-05-01 19:55:48 -0300

    Initial

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args) {
+        var app = Javalin.create(/*config*/)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(8080);
+    }
+}
